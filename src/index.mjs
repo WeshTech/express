d4c03e77181ddd078482086f7f1a9ad56ba7eb05
EngineerDavid <davidwaweru2500@gmail.com>
2025-02-25 12:26:39 +0300

    query strings

--- src/index.mjs
+++ src/index.mjs
@@ -5,7 +5,26 @@ const  PORT = process.env.PORT || 5000;
 
 
 const mockUsers = [
-    {id: 3, userName:"More", email: "davidwaw900@gmail.com"}, 
+    { id: 1, username: "Alice", email: "alice@example.com" },
+    { id: 2, username: "Bob", email: "bob@example.com" },
+    { id: 3, username: "Charlie", email: "charlie@example.com" },
+    { id: 4, username: "David", email: "david@example.com" },
+    { id: 5, username: "Eve", email: "eve@example.com" },
+    { id: 6, username: "Frank", email: "frank@example.com" },
+    { id: 7, username: "Grace", email: "grace@example.com" },
+    { id: 8, username: "Hannah", email: "hannah@example.com" },
+    { id: 9, username: "Isaac", email: "isaac@example.com" },
+    { id: 10, username: "Jack", email: "jack@example.com" },
+    { id: 11, username: "Karen", email: "karen@example.com" },
+    { id: 12, username: "Liam", email: "liam@example.com" },
+    { id: 13, username: "Mia", email: "mia@example.com" },
+    { id: 14, username: "Noah", email: "noah@example.com" },
+    { id: 15, username: "Olivia", email: "olivia@example.com" },
+    { id: 16, username: "Paul", email: "paul@example.com" },
+    { id: 17, username: "Quinn", email: "quinn@example.com" },
+    { id: 18, username: "Ryan", email: "ryan@example.com" },
+    { id: 19, username: "Sophia", email: "sophia@example.com" },
+    { id: 20, username: "Thomas", email: "thomas@example.com" }
 ]
 
 
@@ -15,11 +34,19 @@ app.get('/', (req, res) => {
 
 
 app.get('/api/users', (req, res) => {
-    res.send([
-        {id:1, username: "Kuria"},
-        {id:2, username: "Wendo"},
-        {id:3, username: "Rubia"},
-    ]);
+    console.log(req.query);
+
+    const { 
+        query: {name, value},
+    } = req;
+    //when filter and value are undefined
+    if (!name || !value) return res.send(mockUsers);
+
+    if (name && value) return res.send(
+        mockUsers.filter((user) => user[name].includes(value))
+    )
+
+    res.send(mockUsers);
 });
 
 
@@ -43,7 +70,6 @@ app.get('/api/users/:id', (req, res) => {
     const findUser = mockUsers.find((user) => user.id === parsedId);
     if (!findUser) return res.status(404).send({msg: "Ooops you are not registered on the server"});
     return res.send(findUser);
-
 })
 
 
