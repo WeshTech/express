80a2376f4c2ba7fc9b07657352ea91132f950597
EngineerDavid <davidwaweru2500@gmail.com>
2025-02-26 14:56:41 +0300

    session1

--- src/index.mjs
+++ src/index.mjs
@@ -1,12 +1,26 @@
 import express from "express";
 import routes from './routes/index.mjs';
 import cookieParser from "cookie-parser";
+import session from "express-session";
+import dotenv from "dotenv";
 
+dotenv.config();
 
 const app = express();
 
+const cookieParserSecret = process.env.COOKIE_PARSER_SECRET;
+const sessionSecret = process.env.SESSION_SECRET;
+
 app.use(express.json());
-app.use(cookieParser("BOHh0aOeyCin1zmAulw3Lf8J1ZIJ6pLxOcNxA6Ax45k="));
+app.use(cookieParser(cookieParserSecret));
+app.use(session({
+    secret: sessionSecret,
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: 60000 * 60 * 24,
+    }
+}));
 app.use(routes);
 
 
@@ -14,6 +28,9 @@ const  PORT = process.env.PORT || 5000;
 
 
 app.get('/', (req, res) => {
+    console.log(req.session);
+    console.log(req.session.id);
+    req.session.visited = true;
     res.cookie('hello','world', { maxAge: 60000 * 60 * 2, signed: true });
     res.status(201).send("Welcome to express server programming");
 });
