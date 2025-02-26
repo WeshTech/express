baa765eb81fd26ed72e76353d0825018516a73a9
EngineerDavid <davidwaweru2500@gmail.com>
2025-02-26 12:48:47 +0300

    cookies

--- src/index.mjs
+++ src/index.mjs
@@ -1,16 +1,20 @@
 import express from "express";
-import routes from './routes/index.mjs'
+import routes from './routes/index.mjs';
+import cookieParser from "cookie-parser";
+
 
 const app = express();
 
 app.use(express.json());
-app.use(routes)
+app.use(cookieParser("BOHh0aOeyCin1zmAulw3Lf8J1ZIJ6pLxOcNxA6Ax45k="));
+app.use(routes);
 
 
 const  PORT = process.env.PORT || 5000;
 
 
 app.get('/', (req, res) => {
+    res.cookie('hello','world', { maxAge: 60000 * 60 * 2, signed: true });
     res.status(201).send("Welcome to express server programming");
 });
 
--- src/routes/products.mjs
+++ src/routes/products.mjs
@@ -3,10 +3,16 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/api/products", (req, res) => {
-    res.send([
-        {id:22, productName: "Kuku bar", price: 2349},
-        {id:2, productName: "fufu bar", price: 299},
-    ]);
+    console.log(req.headers.cookie);
+    console.log(req.cookies);
+    if (req.signedCookies.hello && req.signedCookies.hello === "world") {
+        return res.send([
+            {id:22, productName: "Kuku bar", price: 2349},
+            {id:2, productName: "fufu bar", price: 299},
+        ]);
+    }
+    return res.status(403).send({ msg: "Sorry you need the correct cookie!!!" });
+    
 });
 
 export default router;
